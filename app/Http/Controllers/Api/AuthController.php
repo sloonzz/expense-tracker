fc71be4b8733e81cc670d5ea96cf0ecbaa885413
Kendall <kendallrancissegovia@gmail.com>
2018-10-16 23:18:36 +0800

    Added working password grant back-end;

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+}
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
 
 class LoginController extends Controller
 {
@@ -16,7 +18,7 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-    */
+     */
 
     use AuthenticatesUsers;
 
@@ -35,5 +37,35 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $providerUser = Socialite::driver('github')->user();
+
+        // TODO: Register user if it doesn't exist in the database.
+        $user = User::firstOrNew([
+            'name' => $providerUser->name,
+            'password' => bcrypt("secret"),
+            'email' => $providerUser->email,
+        ]);
+        // Otherwise, give the user an access token.
+
+        // $user->token;
     }
 }
