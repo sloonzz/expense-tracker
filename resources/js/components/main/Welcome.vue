2bdeff5907ce7890f231bc59b2a9c12f6c2a30d6
Kendall <kendallrancissegovia@gmail.com>
2018-10-22 01:13:39 +0800

    Minor fix with welcome view

--- resources/js/components/main/Welcome.vue
+++ resources/js/components/main/Welcome.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
-    <h1>
+    <h1 v-if="this.$store.getters.loggedIn">
       Welcome, {{ this.$store.state.user.name }}
+    </h1>
+    <h1 v-else>
+      Create an account now.
     </h1>
   </div>
 </template>
