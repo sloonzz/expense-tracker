2f504310b834b6189b6bc8cd763d4834e593e9a3
Kendall <kendallrancissegovia@gmail.com>
2018-11-22 21:31:26 +0800

    Changed welcome page title.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        <title>Laravel</title>
+        <title>{{env('APP_NAME')}}</title>
     </head>
     <body>
         <div id="app">
