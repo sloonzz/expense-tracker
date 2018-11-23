c7fd8d412ad0326eb45f07efa558028c209e1430
Kendall <kendallrancissegovia@gmail.com>
2018-11-23 09:02:03 +0800

    Reverted login auth controller to original function.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -33,28 +33,26 @@ class AuthController extends Controller
 
         $email = $request->email;
         $password = $request->password;
-        try {
 
-            $request->request->add([
-                'username' => $email,
-                'password' => $password,
-                'grant_type' => 'password',
-                'client_id' => env('PASSWORD_GRANT_CLIENT_ID'),
-                'client_secret' => env('PASSWORD_GRANT_CLIENT_SECRET'),
-                'scope' => '*',
-            ]);
+        $request->request->add([
+            'username' => $email,
+            'password' => $password,
+            'grant_type' => 'password',
+            'client_id' => env('PASSWORD_GRANT_CLIENT_ID'),
+            'client_secret' => env('PASSWORD_GRANT_CLIENT_SECRET'),
+            'scope' => '*',
+        ]);
 
-            $tokenRequest = Request::create(
-                env('APP_URL') . '/oauth/token',
-                'post'
-            );
+        $tokenRequest = Request::create(
+            env('APP_URL') . '/oauth/token',
+            'post',
+            $request->all()
+        );
 
-            $response = Route::dispatch($tokenRequest);
+        $response = Route::dispatch($tokenRequest);
 
-            return $response;
-        } catch (\Exception $e) {
-            return e;
-        }
+        return $response;
+
         // $client = new \GuzzleHttp\Client();
         // $response = $client->request('POST', env('APP_URL') . '/oauth/token', [
         //     [
