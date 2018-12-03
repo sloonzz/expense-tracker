1f062ebf9353c8e3e0f5a43aeb72b0ac244a0a8c
kendallrancissegovia <kendallrancissegovia@gmail.com>
2018-12-03 18:11:38 +0800

    Reverted asset loading to use unsecure for dev.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>{{env('APP_NAME')}}</title>
     </head>
     <body>
         <div id="app">
             <Home></Home>
         </div>
-    <script src="{{secure_asset('js/app.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
     </body>
 </html>
