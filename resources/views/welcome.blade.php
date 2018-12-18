5968e52f0f8e4cd37beb95fb89a44cae1c417b3c
Kendall <kendallrancissegovia@gmail.com>
2018-12-18 12:26:52 +0800

    Fixed conditional secure assets.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @if (env('APP_ENV' == 'production'))
+        @if (env('APP_ENV') == 'production')
             <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         @else
             <link href="{{ asset('css/app.css') }}" rel="stylesheet">            
